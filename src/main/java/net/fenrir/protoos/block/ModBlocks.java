14eca8e071fe2ce26a3eeb7a04e7c452d62f95ae
PupFenrir <dogita231@gmail.com>
2024-09-27 19:12:32 +1000

    commit

--- src/main/java/net/fenrir/protoos/block/ModBlocks.java
+++ src/main/java/net/fenrir/protoos/block/ModBlocks.java
@@ -5,6 +5,7 @@ import net.fenrir.protoos.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SpawnerBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -23,7 +24,6 @@ public class ModBlocks {
             () -> new Block(BlockBehaviour.Properties.of().noOcclusion().explosionResistance(1000000f)));
     public static final RegistryObject<Block> TOPAZ_GLASS_BLOCK = registerBlock("topaz_glass_block",
             () -> new Block(BlockBehaviour.Properties.of().noOcclusion().explosionResistance(1000000f)));
-
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
